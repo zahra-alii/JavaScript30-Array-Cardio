78b0ab4d022239aff68ad9c4f5f764953f176a93
Zahra Ali <zahraali.tech@gmail.com>
2025-04-01 09:53:45 -0400

    Update solution1.js

--- solution1.js
+++ solution1.js
@@ -9,3 +9,7 @@ console.table(fifteen);
 /* SOLUTION EXPLAINED:
 - CB function takes in new variable/object (inventor)
 - create conditions to target inventors born in 1500s using myObject.myMethod(inventor.year) */
+
+// Alternative with arrow function: 
+const  fifteen = inventors.filter(inventor => inventor.year >= 1500 && inventor.year < 1600);
+console.table(fifteen);
